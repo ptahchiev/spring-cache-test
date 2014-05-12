e7795fac51869b062a66e50d0b6768a956118141
Stephane Nicoll <snicoll@gopivotal.com>
2014-05-12 16:34:22 +0200

    Fix use of JSR-107 cache manager

--- src/test/java/com/test/cache/config/TestConfig.java
+++ src/test/java/com/test/cache/config/TestConfig.java
@@ -1,9 +1,13 @@
 package com.test.cache.config;
 
 import javax.annotation.Resource;
+import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
+
+import org.ehcache.jcache.JCacheConfiguration;
 
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
@@ -18,25 +22,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableCaching(mode = AdviceMode.PROXY)
 @ComponentScan(basePackages = { "com.test" })
-public class TestConfig implements CachingConfigurer
+public class TestConfig extends CachingConfigurerSupport
 {
    @Resource
    public ApplicationContext context;
    
    @Override
-   @Bean(name = { "defaultKeyGenerator", "keyGenerator" })
+   @Bean
    public KeyGenerator keyGenerator() {
        return new SimpleKeyGenerator();
    }
    
    @Override
-   @Bean(name = { "defaultCacheManager", "cacheManager" })
+   @Bean
    public CacheManager cacheManager() {
-       final JCacheCacheManager cacheManager = new JCacheCacheManager();
-       cacheManager.setCacheManager((javax.cache.CacheManager) context.getBean("cacheManagerFactoryBean"));
-
-       return cacheManager;
+		return new JCacheCacheManager(jCacheManager());
    }
+
+	protected javax.cache.CacheManager jCacheManager() {
+		javax.cache.CacheManager cacheManager = Caching.getCachingProvider().getCacheManager();
+		final MutableConfiguration<Object, Object> mutableConfiguration
+				= new MutableConfiguration<Object, Object>();
+		mutableConfiguration.setStoreByValue(false); // Otherwise value has to be Serializable
+		cacheManager.createCache("site",
+				new JCacheConfiguration<Object, Object>(mutableConfiguration));
+
+		return cacheManager;
+	}
+
+
 
    @Bean(name = { "defaultCacheManagerFactoryBean", "cacheManagerFactoryBean" })
    protected JCacheManagerFactoryBean defaultCacheManagerFactoryBean() {
